3d1f416aac99353de58462ccac0e8650b18d55cc
rmoongit <emil_tt@mail.ru>
2022-11-21 23:41:00 +0300

    Добавил карточки круизы

--- source/js/modules/burger.js
+++ source/js/modules/burger.js
@@ -1,12 +1,12 @@
 const buttonElement = document.querySelector('[data-burger]');
 const headerElement = document.querySelector('[data-header]');
-const listElement = document.querySelector('[data-list]');
+const navElement = document.querySelector('[data-nav]');
 
 
 const openMenuHandler = () => {
-  buttonElement.classList.toggle('burger--open');
-  headerElement.classList.toggle('page-header--open');
-  listElement.classList.toggle('page-nav__list--open');
+  buttonElement.classList.toggle('is-active');
+  headerElement.classList.toggle('is-active');
+  navElement.classList.toggle('is-active');
 };
 
 const initOpenMenu = () => {
